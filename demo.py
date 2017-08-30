83b24556b32f534a9f2e751a1678a38e1d0c1871
Cardinal <czj237622800@qq.com>
2017-08-30 15:28:55 +0800

    fix bugs again

--- demo.py
+++ demo.py
@@ -14,9 +14,9 @@ ret = _judger.run(max_cpu_time=1000,
                   max_stack=32 * 1024 * 1024,
                   # five args above can be _judger.UNLIMITED
                   exe_path="/root/server_test/main",
-                  input_path="1.in",
-                  output_path="/dev/stdout",
-                  error_path="/dev/stderr",
+                  input_path="in.txt",
+                  output_path="out.txt",
+                  error_path="err.txt",
                   args=[],
                   # can be empty list
                   env=[],
